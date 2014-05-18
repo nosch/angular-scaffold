83f259a385635e7b344a0f354bdb745137eb677a
nosch <norbert.schmidt@mayflower.de>
2014-05-18 20:08:01 +0200

    Implemented concat and minify tasks for JS and CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var html2js = require('gulp-html2js');
+var html2js = require('gulp-html2js')
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var ngmin = require('gulp-ngmin');
+var rev = require('gulp-rev');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 
@@ -8,18 +14,15 @@ var open = require('gulp-open');
 var scaffold = {
     sourceDir: './src/',
     testDir: './test/',
-    bowerDir: './bower_components/',
-    buildDir: './build/',
-    tmpDir: './build/.tmp/',
-    assetDir: './build/.tmp/asset/',
-    concatDir: './build/.tmp/concat/',
-    htmlDir: './build/.tmp/html/',
-    distDir: './build/dist/',
+    distDir: './dist/',
     templateDir: './src/component/templates/'
 };
 
+
+// ---- concat + minify ----
+
 gulp.task('html2js', function() {
-    gulp.src(scaffold.sourceDir + 'view/*.html')
+    return gulp.src(scaffold.sourceDir + 'view/*.html')
         .pipe(html2js({
             base: scaffold.sourceDir,
             outputModuleName: 'application.templates',
@@ -41,6 +44,45 @@ gulp.task('html2js', function() {
         .pipe(gulp.dest(scaffold.templateDir));
 });
 
+/**
+ * @todo replace usemin plugin!
+ */
+gulp.task('concat', ['html2js'], function () {
+    return gulp.src(scaffold.sourceDir + 'index.html')
+        .pipe(usemin({ cssmin: false, htmlmin: false, jsmin: false }))
+        .pipe(gulp.dest(scaffold.distDir));
+});
+
+gulp.task('annotate', ['concat'], function () {
+    return gulp.src(scaffold.distDir + 'js/app.min.js')
+        .pipe(ngmin())
+        .pipe(gulp.dest(scaffold.distDir + 'js/'));
+});
+
+gulp.task('minify-js', ['annotate'], function () {
+    return gulp.src(scaffold.distDir + 'js/*.js')
+        .pipe(uglify({mangle: true}))
+        .pipe(gulp.dest(scaffold.distDir + 'js/'));
+});
+
+gulp.task('minify-css', ['minify-js'], function () {
+    return gulp.src(scaffold.distDir + 'css/*.css')
+        .pipe(minifyCss({keepSpecialComments: 0}))
+        .pipe(gulp.dest(scaffold.distDir + 'css/'));
+});
+
+/**
+ * @todo minify-html does not work
+ */
+gulp.task('minify-html', ['minify-css'], function () {
+    return gulp.src(scaffold.distDir + 'index.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest(scaffold.distDir));
+});
+
+
+// ---- server ----
+
 gulp.task('connect', function() {
     connect.server({
         root: scaffold.sourceDir,
@@ -58,7 +100,7 @@ gulp.task('connect', function() {
 });
 
 gulp.task('reload', function () {
-    gulp.src(scaffold.sourceDir + 'index.html')
+    return gulp.src(scaffold.sourceDir + 'index.html')
         .pipe(connect.reload());
 });
 
@@ -66,4 +108,4 @@ gulp.task('watch', function () {
     gulp.watch([scaffold.sourceDir + '**/*.*'], ['html2js', 'reload']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('server', ['html2js', 'connect', 'watch']);
